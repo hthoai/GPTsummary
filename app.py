5feb9db8a2743290470d1d2a5ad75c7b498041db
hoaiht <hthoai1006@gmail.com>
2024-07-06 15:48:15 +0700

    Rm redundant codes

--- app.py
+++ app.py
@@ -1,62 +1,30 @@
-from typing import Tuple
-
 import streamlit as st
 
-from components.summarizer import Summarizer
-from pages import summarize_book, summarize_doc, summarize_youtube_video
-from resources.prompts import (
-    get_book_system_prompt,
-    get_webpage_summary_prompt,
-    get_youtube_system_prompt,
-)
 from utils import sidebar_options
-
-
-def create_summarizers(
-    llm: object, language: str
-) -> Tuple[Summarizer, Summarizer, Summarizer]:
-    """
-    Create summarizers for different types of content.
-
-    Args:
-        llm (object): The language model object.
-        language (str): The language for summarization.
-
-    Returns:
-        Tuple[Summarizer, Summarizer, Summarizer]: Summarizers for YouTube, books, and documents.
-    """
-    youtube_summarizer = Summarizer(
-        llm=llm, system_prompt=get_youtube_system_prompt(language), language=language
-    )
-    book_summarizer = Summarizer(
-        llm=llm, system_prompt=get_book_system_prompt(language), language=language
-    )
-    doc_summarizer = Summarizer(
-        llm=llm, system_prompt=get_webpage_summary_prompt(language), language=language
-    )
-    return youtube_summarizer, book_summarizer, doc_summarizer
-
 
 if __name__ == "__main__":
     st.set_page_config(
-        page_title="GPTsummary", layout="wide", page_icon="resources/icon.png"
+        page_title="GPTsummary", layout="wide", page_icon="resources/summary.png"
+    )
+    sidebar_options()
+
+    # Create pages
+    document_page = st.Page(
+        page="st_pages/document.py",
+        title="Document",
+        icon=":material/article:",
+    )
+    video_page = st.Page(
+        page="st_pages/youtube_video.py",
+        title="YouTube Video",
+        icon=":material/smart_display:",
+    )
+    book_page = st.Page(
+        page="st_pages/book.py",
+        title="Book",
+        icon=":material/book:",
     )
 
-    llm, language = sidebar_options()
-    youtube_summarizer, book_summarizer, doc_summarizer = create_summarizers(
-        llm, language
-    )
+    page = st.navigation([video_page, document_page, book_page])
 
-    # Create tabs
-    document_tab, video_tab, book_tab = st.tabs(
-        ["📝 Document", "📹 YouTube Video", "📚 Book"]
-    )
-
-    with document_tab:
-        summarize_doc(doc_summarizer)
-
-    with video_tab:
-        summarize_youtube_video(youtube_summarizer)
-
-    with book_tab:
-        summarize_book(book_summarizer)
+    page.run()
