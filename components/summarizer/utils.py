2efb11c9c0c44432083bbc9bb47f8c1e3938f17e
Hoai H. <hthoai1006@gmail.com>
2024-09-29 16:45:31 +0700

    Use LangChain YoutubeLoader

--- components/summarizer/utils.py
+++ components/summarizer/utils.py
@@ -2,47 +2,9 @@ import re
 from typing import Optional
 
 import validators
-from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import WebBaseLoader, YoutubeLoader
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api._errors import NoTranscriptFound, TranscriptsDisabled
-
-
-def extract_video_id_from_url(url):
-    video_id_pattern = r'(?:v=|/v/|youtu\.be/|/embed/|/e/)([^?&"\'>]+)'
-    match = re.search(video_id_pattern, url)
-    if match:
-        return match.group(1)
-    else:
-        raise ValueError("Invalid YouTube URL")
-
-
-def find_transcript(
-    transcript_list, languages: list[str], manually_created: bool = True
-) -> Optional[str]:
-    """Find a transcript in the specified languages.
-
-    Args:
-        transcript_list: The list of available transcripts.
-        languages (list[str]): The list of language codes to search for.
-        manually_created (bool): Whether to search for manually created transcripts.
-
-    Returns:
-        Optional[str]: The transcript string if found, otherwise None.
-    """
-    for lang in languages:
-        try:
-            if manually_created:
-                transcript = transcript_list.find_manually_created_transcript([lang])
-            else:
-                transcript = transcript_list.find_transcript([lang])
-            return get_transcript_string(transcript)
-        except (NoTranscriptFound, TranscriptsDisabled):
-            continue
-    return None
-
-
-def get_transcript_string(transcript):
-    return " ".join([item["text"] for item in transcript.fetch()])
 
 
 def get_youtube_subtitle_string(video_url: str) -> str:
@@ -51,34 +13,16 @@ def get_youtube_subtitle_string(video_url: str) -> str:
     Args:
         video_url (str): The URL of the YouTube video.
     """
-    video_id = extract_video_id_from_url(video_url)
-    if not video_id:
-        return ""
-
     try:
-        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        loader = YoutubeLoader.from_youtube_url(
+            video_url,
+            language=["en", "en-US", "en-UK", "en-GB", "vi", "ja", "ko", "zh-Hans","zh-Hant"],
+        )
+        transcript = loader.load()[0].page_content
+
+        return transcript
     except (NoTranscriptFound, TranscriptsDisabled):
         return ""
-
-    # Define the order of languages to search for transcripts
-    languages_priority = [
-        # Manually created
-        (["en"], True),
-        (["en-US"], True),
-        (["en-UK"], True),
-        (["en-GB"], True),
-        (["vi"], True),
-        # Auto-generated
-        (["en"], False),
-        (["vi"], False),
-    ]
-
-    for languages, manually_created in languages_priority:
-        transcript = find_transcript(transcript_list, languages, manually_created)
-        if transcript:
-            return transcript
-
-    return ""
 
 
 def check_input_type(input_str):
